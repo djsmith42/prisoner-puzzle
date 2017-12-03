683e9efecb25dd667fdf9363d3579acb67600e87
Dave Smith <dsmith@hirevue.com>
2017-12-03 13:30:40 -0500

    Fix bug and report progress

--- simulate.py
+++ simulate.py
@@ -2,17 +2,19 @@ from random import shuffle, seed
 from collections import defaultdict
 from datetime import datetime
 
-ITERATIONS = 100000
+ITERATIONS = 1000000
 PRISONER_COUNT = 100
 
 def main():
     successful_runs = 0
     histogram = defaultdict(int)
-    for _ in xrange(ITERATIONS):
+    for run in xrange(ITERATIONS):
+        if run > 0 and run % 10000 == 0:
+            print "run {} of {} ({}%)".format(run+1, ITERATIONS, run*100/ITERATIONS)
         boxes = generate_boxes(PRISONER_COUNT)
         prisoners_who_found_their_number = sum(find_prisoner_number(prisoner, boxes) for prisoner in range(PRISONER_COUNT))
         histogram[prisoners_who_found_their_number] += 1
-        if prisoners_who_found_their_number >= PRISONER_COUNT/2:
+        if prisoners_who_found_their_number >= PRISONER_COUNT:
             successful_runs += 1
 
     print 'Histogram of successful prisoner counts:'
@@ -20,8 +22,7 @@ def main():
         print '{}\t{}'.format(count, histogram[count])
 
     print 'Successful runs: {} of {} ({}%)'.format(successful_runs, ITERATIONS,
-            round(float(successful_runs)/float(ITERATIONS)*100, 1))
-
+            round(float(successful_runs)/float(ITERATIONS)*100, 3))
 
 def find_prisoner_number(prisoner, boxes):
     number = boxes[prisoner] # starting number
